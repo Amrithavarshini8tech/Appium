c8f02729f0b1618414502fad06617d37632513ea
Amrithavarshini8tech <amrithavarshini8tech@gmail.com>
2025-02-14 14:56:28 -0500

    second commit

--- src/test/java/Amrithavarshini8tech/Appium/BaseSetUp.java
+++ src/test/java/Amrithavarshini8tech/Appium/BaseSetUp.java
@@ -3,25 +3,31 @@ package Amrithavarshini8tech.Appium;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.time.Duration;
 
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
-import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 
 public class BaseSetUp {
 
 	public AndroidDriver driver;
-	@BeforeClass
+
+	@BeforeMethod
 	public void startDriver() throws MalformedURLException, URISyntaxException {
-		UiAutomator2Options options=new UiAutomator2Options();
+		// AppiumCode-> AppiumServer->Device
+		UiAutomator2Options options = new UiAutomator2Options();
 		options.setDeviceName("MediumPhone");
-		options.setApp("C:\\Users\\amrit\\eclipse-workspace2\\Appium\\src\\test\\java\\Amrithavarshini8tech\\Appium\\ApiDemos-debug.apk");
-		driver=new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);				
+		options.setApp(
+				"C:\\Users\\amrit\\eclipse-workspace2\\Appium\\src\\test\\java\\Amrithavarshini8tech\\Appium\\ApiDemos-debug.apk");
+		driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
+		// ImplicitWait
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
-	@AfterClass
+
+	@AfterMethod
 	public void quitDriver() {
 		driver.quit();
 	}
--- src/test/java/Amrithavarshini8tech/Appium/appium.java
+++ src/test/java/Amrithavarshini8tech/Appium/appium.java
@@ -1,19 +1,52 @@
 package Amrithavarshini8tech.Appium;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import com.google.common.collect.ImmutableMap;
 
 import io.appium.java_client.AppiumBy;
 
 public class appium extends BaseSetUp {
-@Test
-public void appiumTest_Preference()  {
-	
-	driver.findElement(AppiumBy.accessibilityId("Preference")).click();
-	driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"3. Preference dependencies\"]")).click();
-	driver.findElement(By.id("android:id/checkbox")).click();
-	driver.findElement(By.xpath("//android.widget.ListView[@resource-id=\"android:id/list\"]/android.widget.LinearLayout[2]/android.widget.RelativeLayout")).click();
-	driver.findElement(By.id("android:id/edit")).sendKeys("Wifi");
-	driver.findElement(By.id("android:id/button1"));
-}
+	@Test
+	public void appiumTest_Preference() {
+		// LOcators: xpath, id, accessibilityId, className, androidUIAutomator
+		// Use accessibilityId & androidUIAutomator with AppiumBy
+		// Recommended to use className with AppiumBy
+		driver.findElement(AppiumBy.accessibilityId("Preference")).click();
+		driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"3. Preference dependencies\"]")).click();
+		driver.findElement(By.id("android:id/checkbox")).click();
+		driver.findElement(By.xpath(
+				"//android.widget.ListView[@resource-id=\"android:id/list\"]/android.widget.LinearLayout[2]/android.widget.RelativeLayout"))
+				.click();
+		String value = driver.findElement(AppiumBy.className("android.widget.TextView")).getText();
+		Assert.assertEquals(value, "WiFi settings");
+		driver.findElement(By.id("android:id/edit")).sendKeys("Wifi");
+		driver.findElement(By.id("android:id/button1"));
+	}
+
+//https://github.com/AppiumTestDistribution/appium-gestures-plugin
+//https://appium.github.io/appium.io/docs/en/writing-running-appium/android/android-mobile-gestures/
+	@Test
+	public void longPress() throws InterruptedException {
+		driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"Views\"]")).click();
+		driver.findElement(AppiumBy.accessibilityId("Expandable Lists")).click();
+		driver.findElement(AppiumBy.accessibilityId("1. Custom Adapter")).click();
+		WebElement ele = driver.findElement(By.xpath("//android.widget.TextView[@text=\"People Names\"]"));
+		((JavascriptExecutor) driver).executeScript("mobile: longClickGesture",
+				ImmutableMap.of("elementId", ((RemoteWebElement) ele).getId(), "duration", 2000));
+		Thread.sleep(1000);
+		// RemoteWebelement is an upper hierarchy, it can accept webelement or mobile
+		// element or any
+		// immutableMap: key value pair, elementid one key and duration is another key(2
+		// key value pairs)
+		driver.findElement(
+				By.xpath("//android.widget.TextView[@resource-id=\"android:id/title\" and @text=\"Sample action\"]"))
+				.click();
+
+	}
 }
